b6d5b7b51c1abc15104ec2565b02bcd5447bc456
huynhngocthanh01 <thanh53845@donga.edu.vn>
2024-10-12 09:46:19 +0700

    init

---
+++ index.js
@@ -0,0 +1,23 @@
+import express  from 'express';
+import morgan  from 'morgan';
+import path  from 'path';
+import ejs  from 'ejs';
+import { fileURLToPath } from 'url';
+
+import routehome from './routerhome.js';
+import routerjob from './router/job.js';
+
+const __dirname = path.dirname(fileURLTopath(import.meta.url));
+
+const app=express();
+const port = 3000;
+
+app.use(morgan("camon"));
+app.set('./views', path.join(__dirname));
+app.engine('html', ejs.renderFile);
+app.set('views engine, 'html');
+app.use(express.static('./public'));
+
+app.use(morgan("camon"));
+
+
