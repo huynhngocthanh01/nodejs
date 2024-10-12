0b53228bd4c4af8843eb6307f432640a3e64e6cf
huynhngocthanh01 <thanh53845@donga.edu.vn>
2024-10-12 11:29:20 +0700

    Update index.js

--- index.js
+++ index.js
@@ -12,12 +12,12 @@ const __dirname = path.dirname(fileURLTopath(import.meta.url));
 const app=express();
 const port = 3000;
 
-app.use(morgan("camon"));
+app.use(morgan("common"));
 app.set('./views', path.join(__dirname));
 app.engine('html', ejs.renderFile);
 app.set('views engine, 'html');
 app.use(express.static('./public'));
 
-app.use(morgan("camon"));
+app.use(morgan("common"));
 
 
